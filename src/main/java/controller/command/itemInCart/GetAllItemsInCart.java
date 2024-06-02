396f1c8045c6f7f0ddc10af9e9c69b0008b7b124
kkatalz <zlata.kiev2004@gmail.com>
2024-06-02 14:09:31 +0300

    Added Basket page.

--- src/main/java/controller/command/itemInCart/GetAllItemsInCart.java
+++ src/main/java/controller/command/itemInCart/GetAllItemsInCart.java
@@ -4,13 +4,15 @@ import entity.ItemInCart;
 import entity.User;
 import service.ItemInCartService;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
+
 
 /**
  * Get all items in cart of current user.
@@ -20,23 +22,27 @@ import java.util.List;
 public class GetAllItemsInCart extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        HttpSession session = request.getSession(false);
-        if(session != null) {
-            User currentUser = (User) session.getAttribute("currentUser");
-            List<ItemInCart> itemsInCart;
-            try {
-                itemsInCart = ItemInCartService.getInstance().getItemsInCartByUserId(currentUser.getId());
-            } catch (Exception e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        HttpSession session = request.getSession(false);
+//        if(session != null) {
+//            User currentUser = (User) session.getAttribute("currentUser");
+//            List<ItemInCart> itemsInCart;
+//            try {
+//                itemsInCart = ItemInCartService.getInstance().getItemsInCartByUserId(currentUser.getId());
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                throw new RuntimeException(e);
+//            }
+//
+//            session.setAttribute("itemsInCart", itemsInCart);
+//            String jspPage = "/WEB-INF/views/basket.jsp";
+//            String redirectURL = request.getContextPath() + jspPage;
+//            response.sendRedirect(redirectURL);
+//        }
 
-            session.setAttribute("itemsInCart", itemsInCart);
-            // TODO: add path to show itemsInCart
-            String jspPage = "/";
-            String redirectURL = request.getContextPath() + jspPage;
-            response.sendRedirect(redirectURL);
-        }
+
+        String jspPage = "/WEB-INF/views/basket.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(jspPage);
+        dispatcher.forward(request, response);
     }
 }
