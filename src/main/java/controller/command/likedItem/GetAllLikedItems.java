670cc83de3af7743ed00e01f0d8c8ac506a46a82
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-06-04 10:23:46 +0200

    show wishlist of current user

--- src/main/java/controller/command/likedItem/GetAllLikedItems.java
+++ src/main/java/controller/command/likedItem/GetAllLikedItems.java
@@ -3,6 +3,7 @@ package controller.command.likedItem;
 import dao.DaoFactory;
 import entity.Item;
 import entity.LikedItem;
+import entity.User;
 import service.ItemService;
 import service.LikedItemService;
 
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,13 +27,20 @@ public class GetAllLikedItems extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        LikedItemService likedItemService = new LikedItemService(DaoFactory.getDaoFactory());
-        List<LikedItem> items = likedItemService.getLikedItemsByUserId(1);
+        HttpSession session = request.getSession(false);
 
-        HttpSession session = request.getSession();
-        session.setAttribute("likedItems", items);
+        if(session != null) {
+            List<LikedItem> items = new ArrayList<>();
+            User user = (User) session.getAttribute("currentUser");
+            LikedItemService likedItemService = LikedItemService.getInstance();
+            if(user != null)
+                items = likedItemService.getLikedItemsByUserId(user.getId());
 
-        System.out.println(items);
+
+            session.setAttribute("likedItems", items);
+            System.out.println(items);
+
+        }
 
         String jspPage = "/WEB-INF/views/wishlist.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(jspPage);
