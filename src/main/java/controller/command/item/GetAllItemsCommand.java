c39352796462f405461733b6f21b2c8307ee4d3c
kkatalz <zlata.kiev2004@gmail.com>
2024-06-04 17:18:33 +0300

    Connected liking/unliking items.

--- src/main/java/controller/command/item/GetAllItemsCommand.java
+++ src/main/java/controller/command/item/GetAllItemsCommand.java
@@ -3,8 +3,11 @@ package controller.command.item;
 import dao.DaoFactory;
 import entity.Category;
 import entity.Item;
+import entity.LikedItem;
+import entity.User;
 import service.CategoryService;
 import service.ItemService;
+import service.LikedItemService;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -23,15 +26,21 @@ public class GetAllItemsCommand extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession();
+
+        User currentUser = (User) session.getAttribute("currentUser");
+        int idUser = currentUser.getId();
+
         ItemService itemService = new ItemService(DaoFactory.getDaoFactory());
         CategoryService categoryService = new CategoryService(DaoFactory.getDaoFactory());
+        LikedItemService likedItemService = new LikedItemService(DaoFactory.getDaoFactory());
         List<Item> items = itemService.getAllItems();
         List<Category> categories = categoryService.getAllCategories();
+        List<LikedItem> likedItems = likedItemService.getLikedItemsByUserId(idUser);
 
-        HttpSession session = request.getSession();
         session.setAttribute("items", items);
         session.setAttribute("categories", categories);
-
+        session.setAttribute("likedItems", likedItems);
 
         String jspPage = "/WEB-INF/views/allItems.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(jspPage);
@@ -47,6 +56,7 @@ public class GetAllItemsCommand extends HttpServlet {
         String fromAgeStr = request.getParameter("fromDate");
         String toAgeStr = request.getParameter("toDate");
 
+
         ItemService itemService = new ItemService(DaoFactory.getDaoFactory());
         CategoryService categoryService = new CategoryService(DaoFactory.getDaoFactory());
         List<Item> items = new ArrayList<>();
@@ -56,16 +66,12 @@ public class GetAllItemsCommand extends HttpServlet {
             items = itemService.searchItemByNameOrId(search);
         }
 
-        BigDecimal fromPrice = fromPriceStr.isEmpty() ? BigDecimal.ZERO : new BigDecimal(fromPriceStr);
-        BigDecimal toPrice = toPriceStr.isEmpty() ? BigDecimal.valueOf(Double.MAX_VALUE) : new BigDecimal(toPriceStr);
-        int fromAge = fromAgeStr.isEmpty() ? 0 : Integer.parseInt(fromAgeStr);
-        int toAge = toAgeStr.isEmpty() ? Integer.MAX_VALUE : Integer.parseInt(toAgeStr);
+        BigDecimal fromPrice = fromPriceStr.isEmpty() ? null : new BigDecimal(fromPriceStr);
+        BigDecimal toPrice = toPriceStr.isEmpty() ? null : new BigDecimal(toPriceStr);
+        Integer fromAge = fromAgeStr.isEmpty() ? 0 : Integer.parseInt(fromAgeStr);
+        Integer toAge = toAgeStr.isEmpty() ? Integer.MAX_VALUE : Integer.parseInt(toAgeStr);
 
         List<Category> selectedCategories = new ArrayList<>();
-
-        System.out.println(categoryIds);
-        System.out.println(fromAge);
-        System.out.println(toAge);
         if (categoryIds != null) {
             for (String categoryId : categoryIds) {
                 Integer id = Integer.parseInt(categoryId);
@@ -74,10 +80,9 @@ public class GetAllItemsCommand extends HttpServlet {
             }
         }
 
-        System.out.println(selectedCategories);
+        // do not filter if there is no filter values
 
-        // do not filter if there is not filter values
-        if (!(selectedCategories.isEmpty() && fromPrice.equals(BigDecimal.ZERO) && toPrice.equals(BigDecimal.valueOf(Double.MAX_VALUE))
+        if (!(selectedCategories.isEmpty() && fromPrice == null && toPrice == null
                 && fromAge == 0 && toAge == Integer.MAX_VALUE)) {
             items = itemService.filterItems(selectedCategories, fromPrice, toPrice, fromAge, toAge);
 
@@ -85,6 +90,14 @@ public class GetAllItemsCommand extends HttpServlet {
 
         HttpSession session = request.getSession();
         session.setAttribute("items", items);
+
+
+        User currentUser = (User) session.getAttribute("currentUser");
+        int idUser = currentUser.getId();
+
+        LikedItemService likedItemService = new LikedItemService(DaoFactory.getDaoFactory());
+        List<LikedItem> likedItems = likedItemService.getLikedItemsByUserId(idUser);
+        session.setAttribute("likedItems", likedItems);
 
         String jspPage = "/WEB-INF/views/allItems.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(jspPage);
